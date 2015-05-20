cbfde8d604109dccef5f9cb930ccd4db30a9ee22
Tomas Votruba <tomas.vot@gmail.com>
2015-05-20 11:37:31 +0200

    tests: missing Latte definition added

--- tests/DI/ModularLatteFiltersExtensionTest.php
+++ tests/DI/ModularLatteFiltersExtensionTest.php
@@ -6,6 +6,7 @@ use Latte\Engine;
 use Nette\DI\Compiler;
 use PHPUnit_Framework_TestCase;
 use Zenify\ModularLatteFilters\DI\ModularLatteFiltersExtension;
+use Zenify\ModularLatteFilters\Exception\DI\MissingLatteDefinitionException;
 use Zenify\ModularLatteFilters\Tests\DI\FiltersProviderSource\MathFilters;
 
 
@@ -15,20 +16,30 @@ class ModularLatteFiltersExtensionTest extends PHPUnit_Framework_TestCase
 	public function testLoadTaggedServices()
 	{
 		$extension = $this->getExtension();
-		$builder = $extension->getContainerBuilder();
+		$containerBuilder = $extension->getContainerBuilder();
 
-		$builder->addDefinition($extension->prefix('someFilters'))
+		$containerBuilder->addDefinition($extension->prefix('someFilters'))
 			->setClass(MathFilters::class);
 
 		$extension->loadConfiguration();
 
-		$latteEngine = $builder->addDefinition('latte.engine')
+		$latteEngine = $containerBuilder->addDefinition('latte.engine')
 			->setClass(Engine::class);
 
 		$this->assertCount(0, $latteEngine->getSetup());
 
 		$extension->beforeCompile();
 		$this->assertCount(1, $latteEngine->getSetup());
+	}
+
+
+	public function testNoLatteDefinition()
+	{
+		$extension = $this->getExtension();
+		$extension->loadConfiguration();
+
+		$this->setExpectedException(MissingLatteDefinitionException::class);
+		$extension->beforeCompile();
 	}
 
 
